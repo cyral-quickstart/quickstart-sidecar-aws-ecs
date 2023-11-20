2a950bfa2b7c8b354c31df5caaacc8a97c59c2d8
Wilson de Carvalho <796900+wcmjunior@users.noreply.github.com>
2023-11-20 11:59:13 -0800

    Update variable comment

--- sidecar_ecs/variables_sidecar.tf
+++ sidecar_ecs/variables_sidecar.tf
@@ -38,7 +38,7 @@ variable "sidecar_ports" {
 }
 
 variable "sidecar_dns_name" {
-  description = "The fully qualified sidecar domain name. If there's no DNS for the sidecar, use the load balancer DNS instead."
+  description = "The fully qualified sidecar domain name. If no DNS for the sidecar is provided, it will assume the load balancer DNS instead."
   type        = string
   default     = ""
 }
